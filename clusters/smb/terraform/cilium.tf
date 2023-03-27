fedcff2fb309cd3968e93d8ed1d1156990be6e87
Hippie Hacker <hh@ii.coop>
2023-03-27 00:50:42 +0000

    Update Cilium Helm Chart Version and add security
    Co-Authored-By: Stephen Heywood <stephen@ii.coop>

--- clusters/smb/terraform/cilium.tf
+++ clusters/smb/terraform/cilium.tf
@@ -9,7 +9,7 @@ data "helm_template" "cilium" {
   name             = "cilium"
   repository       = "https://helm.cilium.io/"
   chart            = "cilium"
-  version          = "1.13.0"
+  version          = "1.13.1"
   description      = "Cilium Bootstrapped via terraform helm_templates injected into talos cluster.inlineManifests"
   create_namespace = true
   skip_tests       = false
@@ -19,6 +19,28 @@ data "helm_template" "cilium" {
     ipam:
       mode: kubernetes
     kubeProxyReplacement: strict
+    securityContext:
+      capabilities:
+        ciliumAgent:
+          - CHOWN
+          - KILL
+          - NET_ADMIN
+          - NET_RAW
+          - IPC_LOCK
+          - SYS_ADMIN
+          - SYS_RESOURCE
+          - DAC_OVERRIDE
+          - FOWNER
+          - SETGID
+          - SETUID
+        cleanCiliumState:
+          - NET_ADMIN
+          - SYS_ADMIN
+          - SYS_RESOURCE
+    cgroup:
+      autoMount:
+        enabled: false
+      hostRoot: /sys/fs/cgroup
     k8sServiceHost: 123.253.177.99
     k8sServicePort: 6443
     EOT
